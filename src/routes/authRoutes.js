efc623701b90e9a50b5313d2c995d27f057dfb2f
Pulpoide <joacolivero@gmail.com>
2025-04-16 01:58:15 -0300

    style(authRoutes): format code

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,25 +6,25 @@ const validatorMiddleware = require('../middlewares/validatorMiddleware');
 const loginLimiter = require('../middlewares/rateLimiter');
 
 router.post(
-    '/signup',
-    authValidator.signup,          
-    validatorMiddleware,           
-    authController.postSignUp      
-  );
-  
-  router.post(
-    '/signin',
-    loginLimiter,
-    authValidator.signin,        
-    validatorMiddleware,
-    authController.postSignIn
-  );
-  
-  router.post(
-    '/send-code',
-    authValidator.sendCode,       
-    validatorMiddleware,
-    authController.sendVerificationCode
-  );
+  '/signup',
+  authValidator.signup,
+  validatorMiddleware,
+  authController.postSignUp
+);
+
+router.post(
+  '/signin',
+  loginLimiter,
+  authValidator.signin,
+  validatorMiddleware,
+  authController.postSignIn
+);
+
+router.post(
+  '/send-code',
+  authValidator.sendCode,
+  validatorMiddleware,
+  authController.sendVerificationCode
+);
 
 module.exports = router;
