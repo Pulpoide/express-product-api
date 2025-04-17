9fd7c27212793787e5b9ede4699010e20c74ff8f
Pulpoide <joacolivero@gmail.com>
2025-04-17 03:19:46 -0300

    feat(user): add new attributes to user model

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isVerified: { type: Boolean, default: false }
+  isVerified: { type: Boolean, default: false },
+  resetPasswordToken: { type: String },
+  resetPasswordExpires: { type: Date }, 
+  lastCodeSentAt: { type: Date } 
 });
 
 module.exports = mongoose.model('User', userSchema);
